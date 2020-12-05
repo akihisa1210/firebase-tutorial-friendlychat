8d55155619b53acb851d4757059f00b780ee94d8
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2020-12-06 02:33:16 +0900

    send notification

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,5 @@
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-messaging.js');
+importScripts('/__/firebase/init.js');
+
+firebase.messaging();
